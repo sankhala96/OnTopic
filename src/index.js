9684f8fddcab982685b316eafe661eb22c76839c
sankhala <sankhalayash96@gmail.com>
2018-02-07 17:00:30 +0530

    testing-9

--- src/index.js
+++ src/index.js
@@ -15,11 +15,14 @@ app.use(bodyParser.json());
 mongoose.Promise = Promise;
 mongoose.connect(process.env.MONGODB_URL, {useMongoClient: true});
 
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+
 app.use("/api/auth", auth);
 app.use("/api/users", users);
 
 app.get('*/',(req,res) => {
-    res.sendFile(path.join(__dirname,'../client/build','index.html'));
+    res.sendFile(path.resolve(__dirname,'../client/build','index.html'));
 });
 
 app.listen(process.env.PORT || 5000, () => console.log('running on localhost 5000'));
